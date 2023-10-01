84b4484d558ad53224a27b4aaf03723d25d3f832
filonik-cmu <filonik@users.noreply.github.com>
2023-10-01 02:14:38 -0400

    Fix publicPath for monaco web workers

--- packages/playground/vite.config.ts
+++ packages/playground/vite.config.ts
@@ -1,15 +1,21 @@
 import { fileURLToPath, URL } from 'node:url'
-
+import path from "node:path"
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import monacoEditorPlugin from 'vite-plugin-monaco-editor'
 
+const publicPath = "monacoeditorwork"
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  //base: '/polytopic/',
   plugins: [
     vue(),
     monacoEditorPlugin({
-      publicPath: "."
+      publicPath,
+      customDistPath(root, buildOutDir) {
+        return path.join(root, buildOutDir, publicPath)
+      }
     }),
   ],
   resolve: {
